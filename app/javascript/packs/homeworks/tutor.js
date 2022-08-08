5fe0f02970b5de9ef8550b6272f4daa8fb8ca023
Bret-koda <bret@kodakollectiv.com>
2022-08-08 10:24:12 +0800

    fix upload button homework show for tutor and quality officer

---
+++ app/javascript/packs/homeworks/tutor.js
@@ -0,0 +1,7 @@
+$('#document_file').change(function(){
+  if ($('#uploadButton').hasClass('disabled')){
+    $('#uploadButton').removeClass('disabled')
+  } else {
+    $('#uploadButton').raddClass('disabled')
+  }
+})
