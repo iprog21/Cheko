d5d439aa4871301fa1813c8d4a04e138b09557ec
Bret-koda <bret@kodakollectiv.com>
2022-08-08 15:53:29 +0800

    index fixes

--- app/javascript/packs/homeworks/new.js
+++ app/javascript/packs/homeworks/new.js
@@ -11,6 +11,35 @@ function getCookie(name) {
   if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
+
+// $.fn.inputFilter = function(callback, errMsg) {
+//   return this.on("input keydown keyup mousedown mouseup select contextmenu drop focusout", function(e) {
+//     if (callback(this.value)) {
+//       // Accepted value
+//       if (["keydown","mousedown","focusout"].indexOf(e.type) >= 0){
+//         $(this).removeClass("input-error");
+//         this.setCustomValidity("");
+//       }
+//       this.oldValue = this.value;
+//       this.oldSelectionStart = this.selectionStart;
+//       this.oldSelectionEnd = this.selectionEnd;
+//     } else if (this.hasOwnProperty("oldValue")) {
+//       // Rejected value - restore the previous one
+//       $(this).addClass("input-error");
+//       this.setCustomValidity(errMsg);
+//       this.reportValidity();
+//       this.value = this.oldValue;
+//       this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+//     } else {
+//       // Rejected value - nothing to restore
+//       this.value = "";
+//     }
+//   });
+// };
+
+
+// $('#homework_subject').numberInput(function(value) {return /^-?\d*$/.test(value); }, "Must be an integer")
+
 $(function(){
   // $('#homework_deadline').datepicker({
   //   format: 'mm/dd/yyyy',
