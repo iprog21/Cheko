e940b1c62830fe648d7c87833f44fb5fc8f1ffad
ynaevaj <yna@kodakollectiv.com>
2023-01-02 11:02:07 +0800

    fixes: mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,7 +111,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.raise_delivery_errors = true 
-  config.action_mailer.default_url_options = { host: 'http://172.104.186.240/', port: 4000 }
+  config.action_mailer.default_url_options = { host: 'http://172.104.186.240', port: 4000 }
   config.action_mailer.delivery_method   = :smtp
 
    config.action_mailer.smtp_settings = {
