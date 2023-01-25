0c22956eb392c3c4c129eabe7b150a7d04f7036f
Bret-koda <bret@kodakollectiv.com>
2023-01-25 14:04:49 +0800

    test: set_form_data to form_data

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -41,12 +41,12 @@ class Manager < ApplicationRecord
     uri = URI('http://172.104.186.240:3000/api/v1/accounts/1/inbox_members')
     params = {
       inbox_id: 1,
-      "users_id[0]": self.agent_id
+      users_id: [self.agent_id]
     }
 
     http = Net::HTTP.new(uri.host, uri.port)
     req = Net::HTTP::Post.new(uri.request_uri)
-    req.set_form_data(params)
+    req.form_data(params)
     req["api_access_token"] = "4E5F2zFPzMrpSbMZHMRAFHWL"
     res = http.request(req)
     result = JSON.parse(res.body)
