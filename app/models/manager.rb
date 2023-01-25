2f64ead95def70b2942243e329c91a25e235d5a4
Bret-koda <bret@kodakollectiv.com>
2023-01-25 11:26:43 +0800

    fix: fix manager url

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -56,7 +56,8 @@ class Manager < ApplicationRecord
   end
 
   def remove_agent
-    uri = URI('http://172.104.186.240:3000/api/v1/accounts/1/agents/')
+    url = "http://172.104.186.240:3000/api/v1/accounts/1/agents/#{self.agent_id}"
+    uri = URI(url)
     params = {
       id: self.agent_id,
     }
