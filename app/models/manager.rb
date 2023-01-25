65bad364418cfc4ef3fd273757a03f1b0ac6589c
Bret-koda <bret@kodakollectiv.com>
2023-01-25 10:51:55 +0800

    build: added assign to inbox

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -37,6 +37,24 @@ class Manager < ApplicationRecord
     self.update(agent_id: result["id"])
   end
 
+  def assign_inbox
+    uri = URI('http://172.104.186.240:3000/api/v1/accounts/1/inbox_members')
+    params = {
+      inbox_id: 1,
+      users_id: [
+        self.agent_id
+      ]
+    }
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req.set_form_data(params)
+    req["api_access_token"] = "4E5F2zFPzMrpSbMZHMRAFHWL"
+    res = http.request(req)
+    result = JSON.parse(res.body)
+    puts "\n\n #{result}\n\n"
+  end
+
   def remove_agent
     uri = URI('http://172.104.186.240:3000/api/v1/accounts/1/agents')
     http = Net::HTTP.new(uri.host, uri.port)
