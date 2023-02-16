ff0864a9b126cfd167c5bb6ff41a195f5615ab4e
ynaevaj <yna@kodakollectiv.com>
2023-02-16 17:43:21 +0800

    updates: popover on user homepage

--- app/controllers/users/professors_controller.rb
+++ app/controllers/users/professors_controller.rb
@@ -46,8 +46,10 @@ class Users::ProfessorsController < Users::UserAppController
         new_prof.set_up_email
       end
 
-      user = current_user
-      ProfToPickJob.set(wait: 2.seconds).perform_later("review_recieved", user)
+      if current_user.present?
+        user = current_user
+        ProfToPickJob.set(wait: 2.seconds).perform_later("review_recieved", user)
+      end
       
       redirect_to users_professors_path
     end
