ac8b430fa9e307de56d82966667877f644a80eb0
Yna Eva <yna@kodakollectiv.com>
2023-08-29 20:01:56 +0800

    fix: removed Math.remove on js homework computations

--- app/javascript/packs/homeworks/admin.js
+++ app/javascript/packs/homeworks/admin.js
@@ -33,57 +33,57 @@ $(function(){
   );
 })
 
-var prio = parseInt(0)
+var prio = parseFloat(0)
 
 if ($('#homework_tutor_skills2').is(':checked')) {
-  var skill = parseInt(100)
+  var skill = parseFloat(100)
   
 } else {
-  var skill = parseInt(0)
+  var skill = parseFloat(0)
 }
 
 if ($('#homework_tutor_samples2').is(':checked')) {
-  var sample = parseInt(100)
+  var sample = parseFloat(100)
 } else {
-  var sample = parseInt(0)
+  var sample = parseFloat(0)
 }
 
 
 if ($('#homework_login_school2').is(':checked')) {
-  var login = parseInt(200)
+  var login = parseFloat(200)
 } else {
-  var login = parseInt(0)
+  var login = parseFloat(0)
 }
 
 
 if ($('#homework_view_bidders2').is(':checked')) {
-  var bid = parseInt(300)
+  var bid = parseFloat(300)
 } else {
-  var bid = parseInt(0)
+  var bid = parseFloat(0)
 }
 
 var new_price = 0
 
 if ($('#homework_words').length){
-  var def_price = parseInt($('#homework_words').val())
-  prio = Math.round(( .30 * def_price));
-  new_price = Math.round((def_price + skill + sample + login + bid))
+  var def_price = parseFloat($('#homework_words').val())
+  prio = ( .30 * def_price);
+  new_price = (def_price + skill + sample + login + bid)
 
   if ($('#homework_priority').is(':checked')) {
-    new_price = Math.round(new_price + prio)
+    new_price = (new_price + prio)
   }
 
   $('#testEstimate').html( '₱' + new_price )
 
   $('#homework_words').on('change', function(){
-    def_price = Math.round(parseInt($('#homework_words').val()) * parseInt($('input[name="homework[tutor_category]"]:checked').data("bal")));
+    def_price = parseFloat($('#homework_words').val()) * parseFloat($('input[name="homework[tutor_category]"]:checked').data("bal"));
 
-    console.log(parseInt($('input[name="homework[tutor_category]"]:checked').data("bal")))
-    prio = Math.round(( .30 * def_price));
-    new_price = Math.round((def_price + skill + sample + login + bid))
+    console.log(parseFloat($('input[name="homework[tutor_category]"]:checked').data("bal")))
+    prio = ( .30 * def_price);
+    new_price = (def_price + skill + sample + login + bid)
 
     if ($('#homework_priority').is(':checked')) {
-      new_price = Math.round(new_price + prio)
+      new_price = (new_price + prio)
     }
 
     $('#testEstimate').html( '₱' + new_price )
@@ -92,24 +92,24 @@ if ($('#homework_words').length){
   var price = $('#homework_price').val();
   var def_price;
   if(price.length > 0){
-    def_price = parseInt(price);
+    def_price = parseFloat(price);
   }
   else{
-    def_price = parseInt(1000);
+    def_price = parseFloat(1000);
   }
   
   new_price = def_price
-  prio = Math.round(( .30 * def_price));
+  prio = ( .30 * def_price);
   console.log(new_price)
   $('#testEstimate').html( '₱' + new_price )
 }
 
 $('input[name="homework[tutor_category]"]').on('change', function(){
-  var new_def_price = Math.round( parseInt(def_price) * parseInt($(this).data("bal")))
-  new_price = Math.round((new_def_price + skill + sample + login + bid))
-  prio = Math.round(( .30 * new_def_price));
+  var new_def_price = parseFloat(def_price) * parseFloat($(this).data("bal"))
+  new_price = (new_def_price + skill + sample + login + bid)
+  prio = ( .30 * new_def_price);
   if ($('#homework_priority').is(':checked')) {
-    new_price = Math.round(new_price + prio)
+    new_price = (new_price + prio)
   }
 
   $('#testEstimate').html( '₱' + new_price )
@@ -121,63 +121,63 @@ console.log($('#homework_priority'))
 $('#homework_priority2').on('change', function(){
   console.log(new_price)
   if ($('#homework_priority2').is(':checked')) {
-    new_price = Math.round(new_price + prio)
+    new_price = (new_price + prio)
     $('#testEstimate').html( '₱' + new_price )
     console.log(new_price)
   } else {
-    new_price = Math.round(new_price - prio)
+    new_price = (new_price - prio)
     $('#testEstimate').html( '₱' + new_price )
   }
 })
 
 $('#homework_login_school2').on('change', function(){
   if ($('#homework_login_school2').is(':checked')) {
-    login = parseInt(200)
-    new_price = Math.round(new_price + login)
+    login = parseFloat(200)
+    new_price = (new_price + login)
     $('#testEstimate').html( '₱' + new_price )
   } else {
-    login = parseInt(200)
-    new_price = Math.round(new_price - login)
-    login = parseInt(0)
+    login = parseFloat(200)
+    new_price = (new_price - login)
+    login = parseFloat(0)
     $('#testEstimate').html( '₱' + new_price )
   }
 })
 
 $('#homework_view_bidders2').on('change', function(){
   if ($('#homework_view_bidders2').is(':checked')) {
-    bid = parseInt(300)
-    new_price = Math.round(new_price + bid)
+    bid = parseFloat(300)
+    new_price = (new_price + bid)
     $('#testEstimate').html( '₱' + new_price )
   } else {
-    bid = parseInt(300)
-    new_price = Math.round(new_price - bid)
-    bid = parseInt(0)
+    bid = parseFloat(300)
+    new_price = (new_price - bid)
+    bid = parseFloat(0)
     $('#testEstimate').html( '₱' + new_price )
   }
 })
 
 $('#homework_tutor_samples2').on('change', function(){
   if ($('#homework_tutor_samples2').is(':checked')) {
-    sample = parseInt(100)
-    new_price = Math.round(new_price + sample)
+    sample = parseFloat(100)
+    new_price = (new_price + sample)
     $('#testEstimate').html( '₱' + new_price )
   } else {
-    sample = parseInt(100)
-    new_price = Math.round(new_price - sample)
-    sample = parseInt(0)
+    sample = parseFloat(100)
+    new_price = (new_price - sample)
+    sample = parseFloat(0)
     $('#testEstimate').html( '₱' + new_price )
   }
 })
 
 $('#homework_tutor_skills2').on('change', function(){
   if ($('#homework_tutor_skills2').is(':checked')) {
-    skill = parseInt(100)
-    new_price = Math.round(new_price + skill)
+    skill = parseFloat(100)
+    new_price = (new_price + skill)
     $('#testEstimate').html( '₱' + new_price )
   } else {
-    skill = parseInt(100)
-    new_price = Math.round(new_price - skill)
-    skill = parseInt(0)
+    skill = parseFloat(100)
+    new_price = (new_price - skill)
+    skill = parseFloat(0)
     $('#testEstimate').html( '₱' + new_price )
   }
 })
@@ -185,5 +185,5 @@ $('#homework_tutor_skills2').on('change', function(){
 $('#homework_min_bid').on('click',function(){
   var min_bid = $('#testEstimate').text().split('₱')
   console.log(min_bid[1]);
-  $('#homework_min_bid').val(parseInt(min_bid[1]))
+  $('#homework_min_bid').val(parseFloat(min_bid[1]))
 })
--- app/javascript/packs/homeworks/new.js
+++ app/javascript/packs/homeworks/new.js
@@ -89,14 +89,14 @@ $('#new_homework').steps({
 
 $('div .steps').addClass("list-group list-group-horizontal")
 
-var prio = parseInt(0)
-var skill = parseInt(0)
+var prio = parseFloat(0)
+var skill = parseFloat(0)
 
-var sample = parseInt(0)
+var sample = parseFloat(0)
 
-var login = parseInt(0)
+var login = parseFloat(0)
 
-var bid = parseInt(0)
+var bid = parseFloat(0)
 
 var new_price = 0
 
@@ -105,22 +105,22 @@ var discount = 0
 if ($('#homework_words').length){
   var def_price = 0
   $('#homework_words').on('change', function(){
-    def_price = Math.round(parseInt($('#homework_words').val()) * parseInt($('input[name="homework[tutor_category]"]:checked').data("bal")));
+    def_price = parseFloat($('#homework_words').val()) * parseFloat($('input[name="homework[tutor_category]"]:checked').data("bal"));
 
-    console.log(parseInt($('input[name="homework[tutor_category]"]:checked').data("bal")))
+    console.log(parseFloat($('input[name="homework[tutor_category]"]:checked').data("bal")))
 
-    prio = Math.round(( .30 * def_price));
-    new_price = Math.round((def_price + skill + sample + login + bid))
+    prio = ( .30 * def_price);
+    new_price = (def_price + skill + sample + login + bid);
 
     if ($('#homework_priority').is(':checked')) {
-      new_price = Math.round(new_price + prio)
+      new_price = new_price + prio
     }
 
     if(new_price >=5000 && new_price <= 9999){
-      discount = Math.round(.10 * new_price)
+      discount = (.10 * new_price)
       new_price = new_price-discount
     }else if(new_price >= 10000){
-      discount = Math.round(.20 * new_price)
+      discount = .20 * new_price
       new_price = new_price - discount
     }
 
@@ -129,16 +129,24 @@ if ($('#homework_words').length){
 } else {
   var def_price = parseFloat(1000);
   new_price = def_price
-  prio = Math.round(( .30 * def_price));
+  prio = ( .30 * def_price);
   $('#testEstimate').html( '₱' + new_price )
 }
 
 $('input[name="homework[tutor_category]"]').on('change', function(){
-  var new_def_price = Math.round( parseInt(def_price) * parseFloat($(this).data("bal")))
-  new_price = Math.round((new_def_price + skill + sample + login + bid))
-  prio = Math.round(( .30 * new_def_price));
+  var new_def_price = (parseFloat(def_price) * parseFloat($(this).data("bal")))
+  new_price = (new_def_price + skill + sample + login + bid)
+  prio = ( .30 * new_def_price);
   if ($('#homework_priority').is(':checked')) {
-    new_price = Math.round(new_price + prio)
+    new_price = (new_price + prio)
+  }
+
+  if(new_price >= 5000 && new_price <= 9999){
+    discount = (.10 * new_price)
+    new_price = new_price - discount
+  }else if(new_price >= 10000){
+    discount = .20 * new_price
+    new_price = new_price - discount
   }
 
   $('#testEstimate').html( '₱' + new_price )
@@ -148,14 +156,14 @@ $('input[name="homework[tutor_category]"]').on('change', function(){
 
 $('#homework_priority').on('change', function(){
   if ($('#homework_priority').is(':checked')) {
-    prio = Math.round(( .30 * new_price));
-    new_price = Math.round(new_price + prio)
+    prio = ( .30 * new_price);
+    new_price = (new_price + prio)
 
     $('#testEstimate').html( '₱' + new_price )
     console.log(new_price);
 
   } else {
-    new_price = Math.round(new_price - prio)
+    new_price = (new_price - prio)
 
     $('#testEstimate').html( '₱' + new_price )
     console.log(new_price);
@@ -164,15 +172,15 @@ $('#homework_priority').on('change', function(){
 
 $('#homework_login_school').on('change', function(){
   if ($('#homework_login_school').is(':checked')) {
-    login = parseInt(200)
-    new_price = Math.round(new_price + login)
+    login = parseFloat(200)
+    new_price = (new_price + login)
 
     $('#testEstimate').html( '₱' + new_price )
   } else {
-    login = parseInt(200)
+    login = parseFloat(200)
     
-    new_price = Math.round(new_price - login)
-    login = parseInt(0)
+    new_price = (new_price - login)
+    login = parseFloat(0)
 
     $('#testEstimate').html( '₱' + new_price )
   }
@@ -180,14 +188,14 @@ $('#homework_login_school').on('change', function(){
 
 $('#homework_view_bidders').on('change', function(){
   if ($('#homework_view_bidders').is(':checked')) {
-    bid = parseInt(300)
-    new_price = Math.round(new_price + bid)
+    bid = parseFloat(300)
+    new_price = (new_price + bid)
 
     $('#testEstimate').html( '₱' + new_price )
   } else {
-    bid = parseInt(300)
-    new_price = Math.round(new_price - bid)
-    bid = parseInt(0)
+    bid = parseFloat(300)
+    new_price = (new_price - bid)
+    bid = parseFloat(0)
 
     $('#testEstimate').html( '₱' + new_price )
   }
@@ -195,18 +203,18 @@ $('#homework_view_bidders').on('change', function(){
 
 $('#homework_tutor_samples').on('change', function(){
   if ($('#homework_tutor_samples').is(':checked')) {
-    sample = parseInt(100)
-    new_price = Math.round(new_price + sample)
+    sample = parseFloat(100)
+    new_price = (new_price + sample)
 
     $('#testEstimate').html( '₱' + new_price )
   } else {
-    sample = parseInt(100)
-    new_price = Math.round(new_price - sample)
-    sample = parseInt(0)
+    sample = parseFloat(100)
+    new_price = (new_price - sample)
+    sample = parseFloat(0)
 
     if(new_price >= 5000){
-      discount = Math.round(.30 * new_price)
-      new_price = Math.round(new_price - discount)
+      discount = (.30 * new_price)
+      new_price = (new_price - discount)
     }
     
     $('#testEstimate').html( '₱' + new_price )
@@ -215,15 +223,15 @@ $('#homework_tutor_samples').on('change', function(){
 
 $('#homework_tutor_skills').on('change', function(){
   if ($('#homework_tutor_skills').is(':checked')) {
-    skill = parseInt(100)
-    new_price = Math.round(new_price + skill)
+    skill = parseFloat(100)
+    new_price = (new_price + skill)
 
 
     $('#testEstimate').html( '₱' + new_price )
   } else {
-    skill = parseInt(100)
-    new_price = Math.round(new_price - skill)
-    skill = parseInt(0)
+    skill = parseFloat(100)
+    new_price = (new_price - skill)
+    skill = parseFloat(0)
 
 
     $('#testEstimate').html( '₱' + new_price )
