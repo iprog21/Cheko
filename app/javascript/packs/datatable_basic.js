dfff6e2d10e29a1f05379b963d4ba6ff8d09e6e8
Yna Eva <yna@kodakollectiv.com>
2023-09-01 18:42:42 +0800

    fix: pagination for datatables

--- app/javascript/packs/datatable_basic.js
+++ app/javascript/packs/datatable_basic.js
@@ -4,23 +4,17 @@ require('datatables.net-dt');
 
 $('#datatable').DataTable({
   "order": [[0, "desc"]],
-  paging: false,
-  searching: false,
   destroy:true
 });
 
 $('.datatableBasic').DataTable({
   "order": [[0, "desc"]],
-  paging: false,
-  searching: false,
   destroy:true
 });
 
 $('.homeworks-scrollX').DataTable({
   scrollX: true,
   "order": [[0, "desc"]],
-  paging: false,
-  searching: false,
   destroy:true
 });
 
