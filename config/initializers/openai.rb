4374c66c7b89643ca01f41edfc2d305c832c663f
Yna Eva <yna@kodakollectiv.com>
2023-09-07 18:10:38 +0800

    removed keys on github

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -2,6 +2,7 @@
 require "ruby/openai"
 
 OpenAI.configure do |config|
-    config.access_token = ENV.fetch('OPENAI_ACCESS_TOKEN')
+    # config.access_token = ENV.fetch('OPENAI_ACCESS_TOKEN')
+    config.access_token = Rails.application.credentials.openai[:accessToken]
     # config.organization_id = ENV.fetch('OPENAI_ORGANIZATION_ID') # Optional.
 end
