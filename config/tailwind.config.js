a028421ba57e1b069ab589b36fe9e555154a52ce
felix1251 <domhack123@gmail.com>
2023-12-29 22:54:30 +0800

    add tailwind config

---
+++ config/tailwind.config.js
@@ -0,0 +1,31 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim,turbo_stream}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "##0096FF",
+        secondary: "#1F1F1D",
+        tertiary: "#292927",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
