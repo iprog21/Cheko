506c2c1ffdc072e66975eb8ea9dc33dac2a5b4d5
ketanbamn11 <ketankatty11bamn@gmail.com>
2024-01-10 16:41:37 +0530

    commented smtp code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,16 +110,16 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.raise_delivery_errors = true 
-  config.action_mailer.default_url_options = { host: "https://beta.chekohomeworkhelp.com"}
-  config.action_mailer.delivery_method   = :smtp
+  # config.action_mailer.raise_delivery_errors = true 
+  # config.action_mailer.default_url_options = { host: "https://beta.chekohomeworkhelp.com"}
+  # config.action_mailer.delivery_method   = :smtp
 
-   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => "cheko.alert@gmail.com",
-    :password             => Rails.application.credentials.mailer[:password],
-    :authentication       => :login,
-    :enable_starttls_auto => true
-  }
+  #  config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :user_name            => "cheko.alert@gmail.com",
+  #   :password             => Rails.application.credentials.mailer[:password],
+  #   :authentication       => :login,
+  #   :enable_starttls_auto => true
+  # }
 end
